74590d8701b6d2d82dc08ebcc4e8950fb01db6ab
Dom <dom.mcdonnell@gmail.com>
2017-07-11 15:17:39 +0100

    testing edge case: floats

--- spec/transactions_spec.rb
+++ spec/transactions_spec.rb
@@ -6,6 +6,9 @@ describe Transactions do
       subject.deposit(100)
       expect { subject.deposit(100) }.to change { subject.transaction_count }.by 1
     end
+    it 'can take floats as deposits' do
+      expect { subject.deposit(100.50) }.to change { subject.transaction_count }.by 1
+    end
   end
 
   describe '#withdrawal' do
@@ -13,6 +16,9 @@ describe Transactions do
       subject.deposit(100)
       subject.withdrawal(50)
       expect { subject.withdrawal(50) }.to change { subject.transaction_count }.by 1
+    end
+    it 'can take floats as withdrawals' do
+      expect { subject.withdrawal(37.50) }.to change { subject.transaction_count }.by 1
     end
   end
 end
